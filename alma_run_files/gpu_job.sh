8a096e2cad98798d7f1052e302e625a103c0046c
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2022-04-25 14:25:10 +0100

    gpu job

--- alma_run_files/gpu_job.sh
+++ alma_run_files/gpu_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=test_gpu
-#SBATCH --output=/home/ofourkioti/Projects/TransMIL_v2/results/20_mag_results.txt
+#SBATCH --output=/home/ofourkioti/Projects/TransMIL_v2/results/fold1.txt
 #SBATCH --error=/home/ofourkioti/Projects/TransMIL_v2/results/test.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
@@ -12,7 +12,7 @@ module load anaconda/3
 source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
 conda activate torch-gpu
 cd /home/ofourkioti/Projects/TransMIL_v2/
-python train.py --stage='train' --config='Camelyon/TransMIL.yaml'  --gpus=0 --fold=0
+python train.py --stage='train' --config='Camelyon/TransMIL.yaml'  --gpus=0 --fold=1
 #python train.py --stage='test' --config='Camelyon/TransMIL.yaml'  --gpus=0 --fold=0
 
 
