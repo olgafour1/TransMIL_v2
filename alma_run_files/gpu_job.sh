d04626fef501ca4f1a61329452e9f842a6c51d18
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2022-09-29 11:28:08 +0100

    test

--- alma_run_files/gpu_job.sh
+++ alma_run_files/gpu_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=test_gpu
-#SBATCH --output=/home/ofourkioti/Projects/TransMIL_v2/results/ovarian_test.txt
+#SBATCH --output=/home/ofourkioti/Projects/TransMIL_v2/results/ovarian_results.txt
 #SBATCH --error=/home/ofourkioti/Projects/TransMIL_v2/results/test.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
@@ -13,7 +13,7 @@ source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
 conda activate torch-gpu
 cd /home/ofourkioti/Projects/TransMIL_v2/
 
-python train.py --stage='train' --config='ovarian/TransMIL.yaml'  --gpus=0 --fold=0
+python train.py --stage='test' --config='ovarian/TransMIL.yaml'  --gpus=0 --fold=0
 #python train.py --stage='test' --config='tcga/TransMIL.yaml'  --gpus=0 --fold=3
 
 
